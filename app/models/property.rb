8ddf62bd81ac292d0118e7db1db24ff5234635d5
Jean Brito <jeanfbrito@gmail.com>
2014-04-03 18:38:50 -0300

    add validation and default values to new properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,6 +5,18 @@ class Property < ActiveRecord::Base
   belongs_to :property_type
   belongs_to :business_type
 
+  validates :district, :property_type, :business_type, presence: true
+
+  after_initialize do
+    if self.new_record?
+      # values will be available for new record forms.
+      self.bedrooms = 0 
+      self.bathrooms = 0
+      self.suits = 0
+      self.garages = 0
+    end
+  end
+
   def get_cover
   	#cover = pictures.where(property_id: id, cover: true).first
     cover = pictures.where(property_id: id).first #just to dev tests
