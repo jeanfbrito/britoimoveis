e279e47e7c2f379474c717ebe09d67be458b3ffe
Jean Brito <jeanfbrito@gmail.com>
2014-05-28 11:22:20 -0300

    Make only cities with content visible on search

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,5 +5,13 @@ class City < ActiveRecord::Base
 
   has_many :properties, through: :districts
 
+  scope :used, -> do
+    all.select{ |x| x.has_proprieties? }
+  end
+
+  def has_proprieties?
+    properties.any?
+  end
+
 
 end
