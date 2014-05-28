c8e93ccb463f7b4a421f835eaf4b5b2764ac2843
Jean Brito <jeanfbrito@gmail.com>
2014-05-28 16:36:41 -0300

    Make only property_type with content visible on search

--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -2,4 +2,12 @@ class PropertyType < ActiveRecord::Base
 
   has_many :properties
 
+  scope :used, -> do
+    all.select{ |x| x.has_proprieties? }
+  end
+
+  def has_proprieties?
+    properties.published.any?
+  end
+
 end
